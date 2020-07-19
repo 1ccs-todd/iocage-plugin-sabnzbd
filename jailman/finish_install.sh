31ad2d01fe514d033b1fb2b8781a7b129c134b2a
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-07-19 23:10:01 +0200

    More plugin compatibility patches

--- jailman/finish_install.sh
+++ jailman/finish_install.sh
@@ -7,16 +7,13 @@ createmount "$1" "${global_dataset_downloads}" /mnt/downloads
 createmount "$1" "${global_dataset_downloads}"/complete /mnt/downloads/complete
 createmount "$1" "${global_dataset_downloads}"/incomplete /mnt/downloads/incomplete
 
-# setup sabnzbd service
-iocage exec "$1" chown -R _sabnzbd:_sabnzbd /config
-iocage exec "$1" sysrc "sabnzbd_enable=YES"
-iocage exec "$1" sysrc "sabnzbd_conf_dir=/config"
-iocage exec "$1" sysrc "sabnzbd_user=_sabnzbd"
-iocage exec "$1" sysrc "sabnzbd_group=_sabnzbd"
-
 # start once to let service write default config
 iocage exec "$1" service sabnzbd start
+
+# Sleep is needed because iocage doesn’t read line by line but rather reads it in all at once
+sleep 5
 iocage exec "$1" service sabnzbd stop
+
 # put our config in place
 iocage exec "$1" sed -i '' -e 's?host = 127.0.0.1?host = 0.0.0.0?g' /config/sabnzbd.ini
 iocage exec "$1" sed -i '' -e 's?download_dir = Downloads/incomplete?download_dir = /mnt/downloads/incomplete?g' /config/sabnzbd.ini
--- post_install.sh
+++ post_install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # Enable and start the service to generate the config file
-sysrc sabnzbd_enable="YES"
-service sabnzbd start 
-# Sleep is needed because iocage doesn’t read line by line but rather reads it in all at once
-sleep 5
-# Edit config to allow outside access
-sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
-# Restart the service for config change to take 
-service sabnzbd restart
+
+# setup sabnzbd service
+chown -R _sabnzbd:_sabnzbd /config
+sysrc "sabnzbd_enable=YES"
+sysrc "sabnzbd_conf_dir=/config"
+sysrc "sabnzbd_user=_sabnzbd"
+sysrc "sabnzbd_group=_sabnzbd"
 
 
